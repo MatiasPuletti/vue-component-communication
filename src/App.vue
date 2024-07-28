fc97c6f2f92f30d571f23daef1ed2c3905944b12
Matias Puletti <pulettimatias@gmail.com>
2024-07-28 22:11:16 +0800

    Scoped styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
+    <!-- <TheHeader /> -->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,22 +12,22 @@
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo,
+    UserInfo
   },
   data() {
     return {
       activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
       },
     };
   },
