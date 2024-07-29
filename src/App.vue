caaba76e89f0aa35491ad4ac34139f428d8ef359
Matias Puletti <pulettimatias@gmail.com>
2024-07-29 09:04:57 +0800

    Implementing teleport

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,8 @@
     <!-- <TheHeader /> -->
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
-    <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
     <keep-alive>
       <component :is="selectedComponent"></component>
     </keep-alive>
@@ -14,29 +13,26 @@
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-// import BadgeList from "./components/BadgeList.vue";
-// import UserInfo from "./components/UserInfo.vue";
-// import CourseGoals from "./components/CourseGoals.vue";
-import ActiveGoals from "./components/ActiveGoals.vue";
-import ManageGoals from "./components/ManageGoals.vue";
+import TheHeader from './components/TheHeader.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    // BadgeList,
-    // UserInfo,
-    // CourseGoals,
     ActiveGoals,
     ManageGoals,
   },
   data() {
     return {
-      selectedComponent: "active-goals",
+      selectedComponent: 'active-goals',
       activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
       },
     };
   },
